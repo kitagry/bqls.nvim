0b07f9e040873065cb057840be159c3cc4bb4cbc
Ryo Kitagawa <kitadrum50@gmail.com>
2024-02-23 23:00:10 +0900

    feat: add listJobHistories command

--- lua/bqls/init.lua
+++ lua/bqls/init.lua
@@ -28,6 +28,8 @@ M.handlers = {
   ['workspace/executeCommand'] = function(err, result, params)
     if params.params.command == 'executeQuery' then
       commands.execute_query_handler(err, result, params)
+    elseif params.params.command == 'listJobHistories' then
+      commands.list_job_history_handler(err, result, params)
     else
       vim.lsp.handlers['workspace/executeCommand'](err, result, params)
     end
